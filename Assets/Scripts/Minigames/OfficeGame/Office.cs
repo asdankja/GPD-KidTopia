d9ef8b1aab45eb3051ae0c0d5667980a31056d5f
asdankja <118950658+asdankja@users.noreply.github.com>
2022-12-08 00:17:13 +0800

    code

---
+++ Assets/Scripts/Minigames/OfficeGame/Office.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Office : MonoBehaviour
+{
+    public GameObject[] datas;
+    public Transform[] pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject i in datas)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
